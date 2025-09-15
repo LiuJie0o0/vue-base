1191c613d61a6ad4c3b18296ff98d80f85ad8b87
LiuJie0o0 <791564901@qq.com>
2025-09-15 15:34:22 +0800

    init

---
+++ 19.单文件组件/Student.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>名字: {{ name }}</h2>
+        <h2>年龄: {{ age }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Student",
+    data() {
+        return {
+            name: "假老练",
+            age: 22
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.name)
+        }
+    }
+}
+</script>
+
+<style></style>
